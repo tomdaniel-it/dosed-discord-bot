a1cd53498f377338f3ab1eaa7b09febeca1dab70
George-Daniel Gagiu <dany_gagiu@yahoo.com>
2018-06-27 02:20:02 +0300

    Populated config.

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    
+    restocks: {
+        update_interval: 120,
+        event_update_interval: 5,
+        event_duration: 120,
+        regions: {
+            eu: "eu", 
+            us: "US", 
+            jpn: "jpn"
+        },
+        url: "https://www.supremecommunity.com/restocks/"
+    }
 };
