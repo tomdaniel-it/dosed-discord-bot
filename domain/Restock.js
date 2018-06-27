522d0dce31119964754f27151ab1130c8e51d042
George-Daniel Gagiu <dany_gagiu@yahoo.com>
2018-06-27 04:36:18 +0300

    Made Restock.js constructor.

--- domain/Restock.js
+++ domain/Restock.js
@@ -1,3 +1,17 @@
-export default class Restock {
+class Restock {
     
+    constructor(id, styleId, name, description, imageURL, timestamp, url, region)
+    {
+        this.id = id;
+        this.styleId = styleId;
+        this.name = name;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.timestamp = timestamp;
+        this.url = url;
+        this.region = region;
+    }
+
 }
+
+module.exports = Restock;
