eb2b903574121bec4b34d42f2c4cef98e51c1d77
tomdaniel-it <tomdaniel.it@gmail.com>
2018-06-27 02:47:54 +0200

    Updated service to class

--- service/RestockService.js
+++ service/RestockService.js
@@ -1,23 +1,26 @@
-module.exports = {
-    // Array of restock items
-    displayedRestocks: [],
+module.exports = class RestockService {
+    
+    constructor() {
+        // Array of restock items
+        this.displayedRestocks = [];
+    }
     
     // Returns all restock items
     // Regions: 'eu', 'us', 'jpn'
-    getRestocksSince: function(timestamp, region) { 
+    getRestocksSince(timestamp, region) { 
         return [];
-    },
+    }
     
     // Returns all new restock items which haven't been displayed yet
     // botStartTime:Date => Time that the bot was started
     // Regions: 'eu', 'us', 'jpn'
-    getNewRestocks: function(botStartTime, region) { 
-        if (displayedRestocks.length === 0) return getRestocksSince(botStartTime, region);
-        return getRestocksSince(displayedRestocks.reduce((max, obj) => obj.timestamp > max ? obj.timestamp : max, data[0].timestamp), region);
-    },
+    getNewRestocks(botStartTime, region) { 
+        if (this.displayedRestocks.length === 0) return this.getRestocksSince(botStartTime, region);
+        return this.getRestocksSince(this.displayedRestocks.reduce((max, obj) => obj.timestamp > max ? obj.timestamp : max, data[0].timestamp), region);
+    }
     
     // Add restocks to displayedRestocks array
-    addDisplayedRestocks: function(restocks) {
-        displayedRestocks = displayedRestocks.concat(restocks);
-    },
+    addDisplayedRestocks(restocks) {
+        this.displayedRestocks = this.displayedRestocks.concat(restocks);
+    }
 };
