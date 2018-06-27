8356ec856b031790ee7141f2eea068cbef584b5b
tomdaniel-it <tomdaniel.it@gmail.com>
2018-06-27 02:38:56 +0200

    Updated service to module.exports

--- service/RestockService.js
+++ service/RestockService.js
@@ -1,21 +1,23 @@
-// Array of restock items
-export let displayedRestocks = [];
-
-// Returns all restock items
-// Regions: 'eu', 'us', 'jpn'
-export function getRestocksSince(timestamp, region) { 
-    return [];
-}
-
-// Returns all new restock items which haven't been displayed yet
-// botStartTime:Date => Time that the bot was started
-// Regions: 'eu', 'us', 'jpn'
-export function getNewRestocks(botStartTime, region) { 
-    if (displayedRestocks.length === 0) return getRestocksSince(botStartTime, region);
-    return getRestocksSince(displayedRestocks.reduce((max, obj) => obj.timestamp > max ? obj.timestamp : max, data[0].timestamp), region);
-}
-
-// Add restocks to displayedRestocks array
-export function addDisplayedRestocks(restocks) {
-    displayedRestocks = displayedRestocks.concat(restocks);
-}
+module.exports = {
+    // Array of restock items
+    displayedRestocks: [],
+    
+    // Returns all restock items
+    // Regions: 'eu', 'us', 'jpn'
+    getRestocksSince: function(timestamp, region) { 
+        return [];
+    },
+    
+    // Returns all new restock items which haven't been displayed yet
+    // botStartTime:Date => Time that the bot was started
+    // Regions: 'eu', 'us', 'jpn'
+    getNewRestocks: function(botStartTime, region) { 
+        if (displayedRestocks.length === 0) return getRestocksSince(botStartTime, region);
+        return getRestocksSince(displayedRestocks.reduce((max, obj) => obj.timestamp > max ? obj.timestamp : max, data[0].timestamp), region);
+    },
+    
+    // Add restocks to displayedRestocks array
+    addDisplayedRestocks: function(restocks) {
+        displayedRestocks = displayedRestocks.concat(restocks);
+    },
+};
