39546ade33640e9fe1d384d74fa52342309f026f
tomdaniel-it <tomdaniel.it@gmail.com>
2018-07-18 20:02:44 +0200

    Fixed small bug

--- domain/UserCommand.js
+++ domain/UserCommand.js
@@ -2,7 +2,7 @@ module.exports = class UserCommand {
     constructor(message) {
         this.message = message;
         this.author = message.author;
-        if (content.length > 0) {
+        if (message.content.length > 0) {
             this.content = message.content.trim().substring(1);
         } else {
             this.content = message.content.trim();
